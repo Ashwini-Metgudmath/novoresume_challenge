66641185c1f9732e1a6c9daacb7186d774bc9a50
unknown <metgudmath.ashwini7@gmail.com>
2020-10-28 11:11:20 +0100

    added invalid user test

--- backend/tests/login.test.js
+++ backend/tests/login.test.js
@@ -9,18 +9,36 @@ chai.should();
 chai.use(chaiHttp);
 
 const user = {
-  email: "guru@gmail.com",
+  email: "ashwini@gmail.com",
   password: "asd",
 };
 
 describe("POST /users/login", () => {
-  it("Should login user and return  token", () => {
+  it("Should login valid user and return  token", () => {
     chai
       .request(server)
       .post("/users/login")
-      .send(user)
+      .send({
+        email : "ash@gmail.com",
+        password : "asd"
+      })
       .end((err, response) => {
         response.should.have.status(200);
+        response.should.have.a('object');
+        response.should.have.property("token");
+      });
+  });
+
+  it("Should reject invalid user and return  403", () => {
+    chai
+      .request(server)
+      .post("/users/login")
+      .send({
+          email : "ash@gmail.com",
+          password : "asd123"
+      })
+      .end((err, response) => {
+        response.should.have.status(403);
       });
   });
 });
